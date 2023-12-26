f0b81e225350ea47bddb0c12a9814cb2122fd9a3
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 01:15:43 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -21,7 +21,7 @@ export async function onRequest(context) {  // Contents of context object
      };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
      // const response = fetch('https://telegra.ph' + urlhz + url.search ,{
-     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph' + urlhz + lx + zlj.value + url.search ,{ 
+     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph' + urlhz + lx + zlj.value + url.pathname.slice(6) + url.search ,{ 
          method: request.method,
          headers: request.headers,
          body: request.body,
