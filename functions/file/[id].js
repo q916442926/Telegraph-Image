f4fa83e1fb91667a8c589da4c3953a0aac5aaf12
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 22:18:29 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,9 +9,8 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
-     console.log(url.search);
-     return;
-    const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+    // const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+    const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=' + url.pathname + url.search + url, {    
          method: request.method,
          headers: request.headers,
          body: request.body,
