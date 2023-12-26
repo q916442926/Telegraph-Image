3774c8cba700f2207f456caabb5a8c31716ff003
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 23:54:54 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -14,7 +14,8 @@ export async function onRequest(context) {  // Contents of context object
     
     
      // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz,{ 
-     const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, {  
+     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
+    const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
          method: request.method,
          headers: request.headers,
          body: request.body,
