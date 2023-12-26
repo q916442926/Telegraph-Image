c988fa504e9f5449b9eaf2a09491a2651437361f
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 00:36:27 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,7 +9,7 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
-     const zlj = await env.img_url.getWithMetadata('d48682cedb94d371246b4.jpg');  //JSON.stringify()
+     const zlj = await env.img_url.getWithMetadata('5f954516455ba3352f8d1.jpg');  //JSON.stringify()
      let urlhz ='LL';
      if (zlj.value === ""){
          urlhz = url.pathname + url.search;
@@ -17,8 +17,8 @@ export async function onRequest(context) {  // Contents of context object
          urlhz = 'file/' + zlj.value;
      };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
-    // const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz ,{ 
+     const response = fetch('https://telegra.ph/' + url.pathname, {
+     // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz ,{ 
          method: request.method,
          headers: request.headers,
          body: request.body,
