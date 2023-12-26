882f3497a3fa944e2c965f52114d8b642a6848ee
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 23:48:16 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -12,10 +12,9 @@ export async function onRequest(context) {  // Contents of context object
      const urlhz = url.pathname
      const zlj = await env.img_url.getWithMetadata('二次元_大腿_修女');//JSON.stringify()
     
-           urlhz = zlj.value; 
     
-     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz,{ 
-     // const response = fetch('https://telegra.ph/' + urlhz + url.search, {  
+     // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz,{ 
+     const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, {  
          method: request.method,
          headers: request.headers,
          body: request.body,
