096e09d0e00890cf76fe0330d6cc3c219c85e00e
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 23:01:34 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,7 +9,7 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
-     const guest = JSON.stringify(await env.img_url.getWithMetadata('二次元_大腿_修女'));
+     const guest = JSON.stringify(await env.img_url.getWithMetadata('5f954516455ba3352f8d1.jpg'));
     // const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=' + guest, {    
          method: request.method,
