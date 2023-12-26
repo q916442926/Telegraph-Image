3a874eac0344bc60a47b9ac07a90da547aa3fd3c
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 00:37:26 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -12,12 +12,12 @@ export async function onRequest(context) {  // Contents of context object
      const zlj = await env.img_url.getWithMetadata('5f954516455ba3352f8d1.jpg');  //JSON.stringify()
      let urlhz ='LL';
      if (zlj.value === ""){
-         urlhz = url.pathname + url.search;
+         urlhz = url.pathname;
      }else{
          urlhz = 'file/' + zlj.value;
      };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
-     const response = fetch('https://telegra.ph/' + url.pathname, {
+     const response = fetch('https://telegra.ph/' + urlhz + url.search, {
      // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz ,{ 
          method: request.method,
          headers: request.headers,
