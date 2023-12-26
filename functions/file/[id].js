1044d5f88561d6e3ea67449094fa7272cc8494cb
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 03:03:22 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -12,7 +12,7 @@ export async function onRequest(context) {  // Contents of context object
      let lujing = url.pathname;
      lujing = lujing.split('/');
      const wenjianming = lujing[2];
-     const zlj = await env.img_url.getWithMetadata(wenjianming );  //JSON.stringify()
+     const zlj = await env.img_url.getWithMetadata(wenjianming);  //JSON.stringify()
      let urlhz ='LL';
      let lx ='lx';
      if (zlj.value == ""){
@@ -24,7 +24,7 @@ export async function onRequest(context) {  // Contents of context object
      };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
      // const response = fetch('https://telegra.ph' + urlhz + url.search ,{
-     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph' + urlhz + url.search,{ 
+     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph' + urlhz + url.search + lx,{ 
          method: request.method,
          headers: request.headers,
          body: request.body,
