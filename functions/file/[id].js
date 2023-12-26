2e9d4ee8bdaab425e0e1d208af4a5f5174ad4f1b
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 00:21:28 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,7 +9,10 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
-     const url = await env.img_url.getWithMetadata('二次元_白丝_粉毛_玉足.jpg');  //JSON.stringify()
+     const zlj = await env.img_url.getWithMetadata('二次元_白丝_粉毛_玉足.jpg');  //JSON.stringify()
+     let ll ='LL';
+     var vv = 'vv';
+         kk = 'kk';
     // if (zlj.value === ""){
     //     const urlhz = url.pathname;
     // }else{
@@ -17,7 +20,7 @@ export async function onRequest(context) {  // Contents of context object
     // };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
     // const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + url.value,{ 
+     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + zlj.value + ll + vv + kk,{ 
          method: request.method,
          headers: request.headers,
          body: request.body,
