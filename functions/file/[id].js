b967cc240e2ed62a80643568b2b7a9dbf1940c4f
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-27 00:38:13 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -14,10 +14,10 @@ export async function onRequest(context) {  // Contents of context object
      if (zlj.value === ""){
          urlhz = url.pathname;
      }else{
-         urlhz = 'file/' + zlj.value;
+         urlhz = '/file/' + zlj.value;
      };
     // const response = fetch('https://telegra.ph/file/' + zlj.value + url.search, { 
-     const response = fetch('https://telegra.ph/' + urlhz + url.search, {
+     const response = fetch('https://telegra.ph' + urlhz + url.search, {
      // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=https://telegra.ph/' + urlhz ,{ 
          method: request.method,
          headers: request.headers,
