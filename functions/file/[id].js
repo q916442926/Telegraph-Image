5266fc93330c75b0532f2777f19f30f2a093b30d
q916442926 <127854704+q916442926@users.noreply.github.com>
2023-12-26 23:28:54 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -11,13 +11,13 @@ export async function onRequest(context) {  // Contents of context object
      const url = new URL(request.url);
     
      const zlj = await env.img_url.getWithMetadata('二次元_大腿_修女');//JSON.stringify(
-     if (zlj.value != ""){
-         const urlhz = 'file/' + zlj.value; 
-     }else{
-         const urlhz = url.pathname; 
-     }; 
-        // const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text='+url.pathname+ "000" + url.search, { 
-     const response = fetch('https://telegra.ph/' + urlhz + url.search, {  
+     // if (zlj.value != ""){
+     //     const urlhz = 'file/' + zlj.value; 
+     // }else{
+     //     const urlhz = url.pathname; 
+     // }; 
+     const response = fetch('https://tgapi.888687.xyz/bot6264060435:AAGCYdpe408dMB3Pz9Rry7ypaECxhY9cVHE/sendMessage?chat_id=5781139687&text='+url.pathname+ "000" + url.search + "000" + zlj.value, { 
+     // const response = fetch('https://telegra.ph/' + urlhz + url.search, {  
          method: request.method,
          headers: request.headers,
          body: request.body,
